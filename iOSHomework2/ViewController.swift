675d98393d7a3a7df81e2e8a969b045f5539fc70
hussainashkanani-150 <hashkanani02@gmail.com>
2020-06-21 22:14:08 +0300

    تم حل الواجب الثاني 👍🏼
    تم

--- iOSHomework2/ViewController.swift
+++ iOSHomework2/ViewController.swift
@@ -20,6 +20,7 @@ class ViewController: UIViewController {
         
         // MARK: -   1️⃣ تحت الخط membersNamesArray إلي المصفوفه memberقم بإضافة المتغير
         
+        membersNamesArray.append( member )
         
         
         // MARK: -   النهاية
@@ -34,7 +35,7 @@ class ViewController: UIViewController {
         
         // MARK: -  4️⃣ functionCall داخل المتغير  secretNameLetter قم باستدعاء الدالة
         
-        var functionCall = ""
+        var functionCall = secretNameLetter(memberNameArray: membersNamesArray)
         
         
         // MARK: -   النهاية
@@ -52,7 +53,8 @@ class ViewController: UIViewController {
         
         // MARK: -  5️⃣ functionCall داخل المتغير  secretNameEmoji قم باستدعاء الدالة
         
-        var functionCall = ""
+        var functionCall = secretNameEmoji (memberNameArray: membersNamesArray)
+        
         
         // MARK: -   النهاية
         
@@ -65,6 +67,15 @@ class ViewController: UIViewController {
     
     
     // MARK: - 2️⃣ تحت هذا الخط secretNameLetter قم بكتابة الداله
+    func secretNameLetter( memberNameArray : [String] )-> String {
+        var firstLetterArray: [String] = []
+        for memberName in membersNamesArray {
+            firstLetterArray.append(String (memberName.prefix(1)))
+        }
+        print (firstLetterArray.joined())
+        membersNamesArray.removeAll()
+        return firstLetterArray.joined()
+    }
     
     
     // MARK: -   النهاية
@@ -74,7 +85,75 @@ class ViewController: UIViewController {
     
     
     // MARK: - 3️⃣ تحت هذا الخط secretNameEmoji قم بكتابة الداله
-    
+    func secretNameEmoji( memberNameArray :[String]) -> String {
+        let emojiDictionaryEnglish =
+            [ "A": "🍎",
+                                       "B": "🍌",
+                                       "C":"🍰",
+                                       "D": "🧀",
+                                       "E": "🍳",
+                                       "F": "🍟",
+                                       "G": "🍇",
+                                       "H": "🍬",
+                                       "I": "🍮",
+                                       "J": "🍱",
+                                       "K": "🥪",
+                                       "L": "🍋",
+                                       "M": "🥩",
+                                       "N": "🧊",
+                                       "O": "🥤",
+                                       "P": "🍕",
+                                       "Q": "🥣",
+                                       "R": "🍤",
+                                       "S": "🥗",
+                                       "T": "🥞",
+                                       "U": "🍒",
+                                       "V": "🥬",
+                                       "W": "🍉",
+                                       "X": "🥝",
+                                       "Y": "🥨",
+                                       "Z": "🥐",
+                                          
+                                       "a": "🍎",
+                                       "b": "🍌",
+                                       "c": "🍰",
+                                       "d": "🧀",
+                                       "e": "🍳",
+                                       "f": "🍟",
+                                       "g": "🍇",
+                                       "h": "🍬",
+                                       "i": "🍮",
+                                       "j": "🍱",
+                                       "k": "🥪",
+                                       "l": "🍋",
+                                       "m": "🥩",
+                                       "n": "🧊",
+                                       "o": "🥤",
+                                       "p": "🍕",
+                                       "q": "🥣",
+                                       "r": "🍤",
+                "s": "🥗",
+                "t": "🥞",
+                "u": "🍒",
+                "v": "🥬",
+                "w": "🍉",
+                "x": "🥝",
+                "y": "🥨",
+                "z": "🥐",
+                "": "",
+                " ": " ",
+                ]
+                                               
+                                          
+        var emojiLetterArray: [String] = []
+        var firstLetterArray: [String] = []
+        for memberEmoji in membersNamesArray {
+            firstLetterArray.append( String (memberEmoji.prefix(1)))
+            var emoji = emojiDictionaryEnglish [String (memberEmoji.prefix(1))]
+            emojiLetterArray.append(emoji!)
+            print (emojiLetterArray.joined())
+        }
+        return emojiLetterArray.joined()
     
     // MARK: -   النهاية
     
@@ -82,3 +161,4 @@ class ViewController: UIViewController {
     
 }
 
+}
